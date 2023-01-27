174757e98861c42b160040a77f14c294c92a54dc
Megan Smith <megans766@gmail.com>
2023-01-27 13:23:20 -0500

    create starship details route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom'
 import StarshipList from './pages/StarshipList/StarshipList'
+import StarshipDetails from './pages/StarshipDetails/StarshipDetails';
 
 function App() {
   return (
     <>
       <Routes>
         <Route path='/starships' element={<StarshipList />} />
+        <Route path='/starships/:id' element={<StarshipDetails />} />
       </Routes>
     </>
   )
---
+++ src/pages/StarshipDetails/StarshipDetails.jsx
@@ -0,0 +1,10 @@
+import { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+const StarshipDetails = () => {
+  return (
+
+  )
+}
+
+export default StarshipDetails
