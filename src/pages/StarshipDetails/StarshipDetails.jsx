1c94b17a2445bdbf98e72f7542a5313ec6f59795
Megan Smith <megans766@gmail.com>
2023-01-27 13:31:12 -0500

    create starship details state

--- src/pages/StarshipDetails/StarshipDetails.jsx
+++ src/pages/StarshipDetails/StarshipDetails.jsx
@@ -1,9 +1,21 @@
 import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import { getStarshipDetails } from "../../services/sw-api";
 
 const StarshipDetails = () => {
-  return (
+  const [starshipDetails, setStarshipDetails] = useState({})
+  const location = useLocation()
 
+  useEffect(() => {
+    const fetchStarshipDetails = async () => {
+      const starshipData = await getStarshipDetails(location.state.starship.url)
+      setStarshipDetails(starshipData)
+    }
+    fetchStarshipDetails()
+  }, [location.state.starship.url])
+
+  return (
+    
   )
 }
 
--- src/services/sw-api.js
+++ src/services/sw-api.js
@@ -4,3 +4,8 @@ export async function getAllStarships() {
   const res = await fetch(`${baseUrl}/starships`)
   return res.json()
 }
+
+export async function getStarshipDetails() {
+  const res = await fetch(`${baseUrl}/starships/:id`)
+  return res.json()
+}
