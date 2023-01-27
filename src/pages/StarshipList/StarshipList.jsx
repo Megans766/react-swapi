aa52587918e3f8ccd8fd64c45257429fee70452d
Megan Smith <megans766@gmail.com>
2023-01-27 13:10:20 -0500

    create starship state & routes

---
+++ src/pages/StarshipList/StarshipList.jsx
@@ -0,0 +1,37 @@
+import { getAllStarships } from '../../services/sw-api'
+import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+
+
+const StarshipList = () => {
+  const [ starshipList, setAllStarships ] = useState([])
+
+  useEffect(() => {
+    const fetchAllStarships = async () => {
+      const starshipData = await getAllStarships()
+      setAllStarships(starshipData.results)
+    }
+    fetchAllStarships()
+  }, [])
+
+  return (
+    <>
+      <h3>Starships</h3>
+      {starshipList.length ?
+        <>
+          {starshipList.map(starship =>
+              <Link to='/starships' state={{starship}} key={starship.name}>
+                {starship.name}
+              </Link>
+          )}
+        </>
+        :
+        <>
+          <h3>Incoming from another galaxy...</h3>
+        </>
+      }
+    </>
+  )
+}
+
+export default StarshipList
